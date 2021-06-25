373f3bdc0446072765a3cdffaf63156b369bec20
João Lucas Bernardi <joao_bernardi@outlook.com>
2021-06-24 23:19:28 -0300

    Implement API

--- web/servers/api.py
+++ web/servers/api.py
@@ -12,21 +12,21 @@ def api_http(event):
     # Check the request host
     if "Host" in request.headers and request.headers["Host"] == config.scopes["api"]:
         # Placeholder response
-        if event.path[0] == "place":
+        if event.path[0] == "places":
             # Query data from the database
-            if event.path == ["place", "query"]:
+            if event.path == ["places", "query"]:
                 output = {"status": 500, "message": "Not Implemented", "error": True}
 
             # Insert data into the database
-        elif event.path == ["place", "insert"]:
+        elif event.path == ["places", "insert"]:
                 output = {"status": 500, "message": "Not Implemented", "error": True}
 
             # Edit place data
-        elif event.path == ["place", "edit"]:
+        elif event.path == ["places", "edit"]:
                 output = {"status": 500, "message": "Not Implemented", "error": True}
 
             # Query the sources from the database
-        elif event.path == ["place", "sources"]:
+        elif event.path == ["places", "sources"]:
                 output = {"status": 500, "message": "Not Implemented", "error": True}
 
         jsonfied = json.dumps(output).encode()
