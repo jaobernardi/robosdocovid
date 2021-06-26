b898f5d0cdc6a59f6bb4f9f239f220d46f30cc9f
João Lucas Bernardi <joao_bernardi@outlook.com>
2021-06-26 11:29:56 -0300

    Add Traceback

--- web/structures/http.py
+++ web/structures/http.py
@@ -79,7 +79,7 @@ class Server(object):
 		except Exception as e:
 		    exc_type, exc_value, exc_traceback = sys.exc_info()
 		    print("*** print_tb:")
-		    traceback.print_tb(exc_traceback, limit=1, file=sys.stdout)
+		    traceback.print_exception(exc_type, exc_value, exc_traceback, limit=2, file=sys.stdout)
 		conn.close()
 
 		pass
