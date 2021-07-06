99b06fbf80df528885eb86096f34aa691c7a9a92
João Lucas Bernardi <joao_bernardi@outlook.com>
2021-07-06 15:26:01 -0300

    Finish worker

--- web/worker/worker.py
+++ web/worker/worker.py
@@ -1,9 +1,14 @@
-from .dataset_collections import PR_Collect, SC_Collect, RS_Collect
+from .dataset_collections import PR_Collect, SC_Collect, RS_Collect, National_Collect
+import time
 
 def run():
-	PR_Collect()
-	print("Done PR")
-	RS_Collect()
-	print("Done RS")
-	SC_Collect()
-	print("Done")
+	while True:
+		PR_Collect()
+		print("Done PR")
+		RS_Collect()
+		print("Done RS")
+		SC_Collect()
+		print("Done SC")
+		National_Collect()
+		print("Done")
+		time.sleep(30*60)
