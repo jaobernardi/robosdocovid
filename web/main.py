000810d9a621047e1e3e7bbeb9ce23785dd44a3a
João Lucas Bernardi <joao_bernardi@outlook.com>
2021-07-06 15:28:21 -0300

    Install worker

--- web/main.py
+++ web/main.py
@@ -1,11 +1,15 @@
 from structures import Server, Config
 import servers
 import events
+from worker.worker import run
+from tasks import add_process
 
+add_process("Worker", run)
 events.call_event("startup")
 config = Config()
 server = Server(config.host, config.port, config.certificate, config.private_key)
 server.https_start()
+
 def shutdown(*args, **kwargs):
     server.stop()
     exit()
